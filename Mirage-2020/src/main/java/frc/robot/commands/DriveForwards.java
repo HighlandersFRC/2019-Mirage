c5792e7ea8b55ca14ee484ecb0d73ac91b6019d5
LAPTOP-OM\omvis <omvishraj@gmail.com>
2020-08-30 17:49:33 -0600

    Updated F Values for DriveTrain MotionMagic

--- Mirage-2020/src/main/java/frc/robot/commands/DriveForwards.java
+++ Mirage-2020/src/main/java/frc/robot/commands/DriveForwards.java
@@ -9,41 +9,56 @@ package frc.robot.commands;
 
 import com.ctre.phoenix.motorcontrol.ControlMode;
 
-import edu.wpi.first.wpilibj2.command.CommandBase;
+import edu.wpi.first.wpilibj.RobotState;
+import edu.wpi.first.wpilibj.command.Command;
+import frc.robot.ButtonMap;
+import frc.robot.OI;
 import frc.robot.RobotMap;
-import frc.robot.tools.controlLoops.PID;
+import frc.robot.RobotStats;
 
-public class DriveForwards extends CommandBase {
- 
-  /**
-   * 
-   * Creates a new DriveForwards.
-   */
+public class DriveForwards extends Command {
   public DriveForwards() {
+    // Use requires() here to declare subsystem dependencies
+    // eg. requires(chassis);
+  }
+
+  // Called just before this Command runs the first time
+  @Override
+  protected void initialize() {
+    RobotMap.leftMaster.selectProfileSlot(0,0);
+    RobotMap.rightMaster.selectProfileSlot(0,0);
+  }
+
+
+  // Called repeatedly when this Command is scheduled to run
+  @Override
+  protected void execute() {
+    RobotMap.leftMaster.set(ControlMode.MotionMagic, RobotStats.inchesToTicks(24));
+    RobotMap.rightMaster.set(ControlMode.PercentOutput, RobotStats.inchesToTicks(24));
+
+  }
+
+  // Make this return true when this Command no longer needs to run execute()
+ 
+
+  // Called once after isFinished returns true
+  @Override
+  protected void end() {
     
-    // Use addRequirements() here to declare subsystem dependencies.
   }
 
-  // Called when the command is initially scheduled.
-  @Override
-  public void initialize() {
-    
-
-  }
-  
-  // Called every time the scheduler runs while the command is scheduled.
-  @Override
-  public void execute() {
-  }
-
-  // Called once the command ends or is interrupted.
-  @Override
-  public void end(boolean interrupted) {
-  }
-
-  // Returns true when the command should end.
   @Override
   public boolean isFinished() {
+    if(OI.driverController.getXButton()){
+      return true;
+    }
     return false;
   }
+
+  // Called when another command which requires one or more of the same
+  // subsystems is scheduled to run
+  @Override
+  protected void interrupted() {
+  }
 }
+
